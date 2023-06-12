98b5b4a661c01142ec70f245ee47cd4b5edb70ae
Adeola Adelakun <aadelakun28@gmail.com>
2023-06-12 09:02:37 +0000

    wallet page added

--- vesting-interface/pages/index.js
+++ vesting-interface/pages/index.js
@@ -1,40 +1,85 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import 'tailwindcss/tailwind.css'
+import React, { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import 'tailwindcss/tailwind.css';
 
-export default function Home() {
+const WalletCard = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [userBalance, setUserBalance] = useState(null);
+  const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+
+  const connectWalletHandler = () => {
+    if (window.ethereum && window.ethereum.isMetaMask) {
+      console.log('MetaMask Here!');
+
+      window.ethereum
+        .request({ method: 'eth_requestAccounts' })
+        .then((result) => {
+          accountChangedHandler(result[0]);
+          setConnButtonText('Wallet Connected');
+          getAccountBalance(result[0]);
+        })
+        .catch((error) => {
+          setErrorMessage(error.message);
+        });
+    } else {
+      console.log('Need to install MetaMask');
+      setErrorMessage('Please install MetaMask browser extension to interact');
+    }
+  };
+
+  const accountChangedHandler = (newAccount) => {
+    setDefaultAccount(newAccount);
+    getAccountBalance(newAccount.toString());
+  };
+
+  const getAccountBalance = (account) => {
+    window.ethereum
+      .request({ method: 'eth_getBalance', params: [account, 'latest'] })
+      .then((balance) => {
+        setUserBalance(ethers.utils.formatEther(balance));
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  };
+
+  const chainChangedHandler = () => {
+    // reload the page to avoid any errors with chain change mid use of application
+    window.location.reload();
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.ethereum.on('accountsChanged', accountChangedHandler);
+      window.ethereum.on('chainChanged', chainChangedHandler);
+    }
+  }, []);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Token Vesting DApp</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://github.adesdesk.io">Happy Vesting!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          
+    <div className="flex flex-col items-center justify-center min-h-screen bg-green-200">
+      <h4 className="text-2xl font-bold my-8">Connect Metamask</h4>
+      <button
+        className="px-4 py-2 bg-blue-500 text-white rounded-md mb-4"
+        onClick={connectWalletHandler}
+      >
+        {connButtonText}
+      </button>
+      <div className="bg-white p-4 rounded-md shadow-lg mb-4">
+        <h3 className="text-lg font-semibold mb-2">Connected Wallet Address:</h3>
+        <p className="text-gray-700">{defaultAccount}</p>
+      </div>
+      {/*<div className="bg-white p-4 rounded-md shadow-lg mb-4">
+        <h3 className="text-lg font-semibold mb-2">Balance:</h3>
+        <p className="text-gray-700">{userBalance}</p>
+  </div>
+      {errorMessage && (
+        <div className="bg-red-500 text-white p-4 rounded-md shadow-lg mb-4">
+          {errorMessage}
         </div>
-      </main>
+      )}*/}
+    </div>
+  );
+};
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-          </div>
-  )
-}
+export default WalletCard;
