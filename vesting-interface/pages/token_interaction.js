febb5cc5257dc03c2ba69878129551440bd6c539
Adeola Adelakun <aadelakun28@gmail.com>
2023-06-12 10:58:31 +0000

    add token abi

--- vesting-interface/pages/token_interaction.js
+++ vesting-interface/pages/token_interaction.js
@@ -10,8 +10,8 @@ const TokenInteraction = ({ WalletCard }) => {
 
   const handleTransfer = async () => {
     try {
-      const contractAddress = 'CONTRACT_ADDRESS'; // Replace with the actual contract address
-      const contractABI = ['ABI_ARRAY']; // Replace with the actual contract ABI
+      const contractAddress = '0x196A4868d590afc7C6DDc6c67bE738DE97aAcd33';
+      const contractABI = require('../contractABIs/CompanyToken.json'); // Replace with the actual contract ABI
 
       // Create an instance of the contract using the wallet's provider
       const contract = new ethers.Contract(contractAddress, contractABI, WalletCard.provider);
@@ -46,35 +46,48 @@ const TokenInteraction = ({ WalletCard }) => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-green-200">
-      <h4>Token Interaction</h4>
-      <div>
-        <h5>Transfer Tokens</h5>
+      <h4 className="text-2xl font-bold mb-4">Token Interaction</h4>
+      <div className="mb-4">
+        <h5 className="text-lg font-semibold mb-2">Transfer Tokens</h5>
         <input
+          className="border border-black rounded-md py-2 px-4 mb-2 w-full"
           type="text"
           placeholder="Recipient address"
           value={transferTo}
           onChange={(e) => setTransferTo(e.target.value)}
         />
         <input
+          className="border border-black rounded-md py-2 px-4 mb-2 w-full"
           type="text"
           placeholder="Amount"
           value={transferAmount}
           onChange={(e) => setTransferAmount(e.target.value)}
         />
-        <button onClick={handleTransfer}>Transfer</button>
+        <button
+          className="bg-blue-500 text-white rounded-md py-2 px-4"
+          onClick={handleTransfer}
+        >
+          Transfer
+        </button>
       </div>
       <div>
-        <h5>Approve Spending</h5>
+        <h5 className="text-lg font-semibold mb-2">Approve Spending</h5>
         <input
+          className="border border-black rounded-md py-2 px-4 mb-2 w-full"
           type="text"
           placeholder="Amount"
           value={approvalAmount}
           onChange={(e) => setApprovalAmount(e.target.value)}
         />
-        <button onClick={handleApproval}>Approve</button>
+        <button
+          className="bg-blue-500 text-white rounded-md py-2 px-4"
+          onClick={handleApproval}
+        >
+          Approve
+        </button>
       </div>
     </div>
-  );
+  );  
 };
 
 export default TokenInteraction;
