1f65dc7b0aac740506402e20662414294bf727e2
Adeola David Adelakun <aadelakun28@gmail.com>
2023-06-14 22:45:27 +0100

    update contract ABIs

---
+++ vesting-interface/pages/register_org_token.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import 'tailwindcss/tailwind.css';
+import { connectWallet, getAccountBalance } from './wallet';
+import { ethers } from 'ethers';
+const Token = require('../contractABIs/TokenContract.json');
+
+const RegisterOrganizationToken = () => {
+  
+};
+
+export default RegisterOrganizationToken;
